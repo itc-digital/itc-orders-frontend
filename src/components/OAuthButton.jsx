9de7a6c339b14616df7d0a964313d63ef6d9246a
Mikhail Malyshev <mlshv42@ya.ru>
2018-01-24 22:32:18 +0300

    Приукрашен блок с кнопкой авторизации

--- src/components/OAuthButton.jsx
+++ src/components/OAuthButton.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Button from 'rambler-ui/Button';
 import { oAuthUrl } from 'services/api/constants';
 
-const OAuthButton = () => <Button href={oAuthUrl} target="_blank">Вход</Button>;
+const OAuthButton = ({ children }) => (
+    <Button href={oAuthUrl} target="_blank">
+        {children || 'Вход'}
+    </Button>
+);
 
 export default OAuthButton;
--- src/utils/withAuth.jsx
+++ src/utils/withAuth.jsx
@@ -3,9 +3,17 @@ import { connect } from 'react-redux';
 import { roles } from 'services/auth/constants';
 import { selectors } from 'services/auth/reducer';
 import OAuthButton from 'components/OAuthButton';
+import { H3 } from 'rambler-ui/Typography';
+
+const NeedAuthNotice = () => (
+    <div>
+        <H3>Для продолжения необходимо зарегистрироваться или войти</H3>
+        <OAuthButton>Продолжить</OAuthButton>
+    </div>
+);
 
 const withAuth = Component => props =>
-    (props.authRole === roles.USER ? <Component {...props} /> : <OAuthButton />);
+    (props.authRole === roles.USER ? <Component {...props} /> : <NeedAuthNotice />);
 
 const mapStateToProps = state => ({
     authRole: selectors.authRole(state),
